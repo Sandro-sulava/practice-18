0d63e295d833007dc952829b5565c04ddbb1c206
Sandro sulava <sandrosulava2@gmail.com>
2025-05-28 19:49:16 +0400

    feature

---
+++ script.js
@@ -0,0 +1,15 @@
+let invalidStr = "Hello";
+let num = 10;
+
+console.log(num - invalidStr);
+
+let b = 20,
+  c = 30,
+  d = 40;
+
+console.log(b, c, d);
+
+let x = 5;
+let y = 2;
+
+console.log(x % y);
